e6a6152c0383aafb7560ac83609fb8841860a4f4
nhukhanh <nhukhanhqlktb@gmail.com>
2020-04-29 17:07:58 +0700

    add record user

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -7,6 +7,12 @@ module.exports.postCreate = function(req, res, next) {
 	if (!req.body.phone) {
 		errors.push('Phone is required!');
 	}
+	if (!req.body.email) {
+		errors.push('Email is required!');
+	}
+	if (!req.body.password) {
+		errors.push('Password is required!');
+	}
 	if (errors.length) {
 		res.render('users/create', {
 			errors: errors,
